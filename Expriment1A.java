cd896af42e81d67b6894816db46121c27bc4502c
Sujal <sjlchoudhari@gmail.com>
2023-04-24 09:44:42 +0000

    class files added for all

--- Expriment1A.java
+++ Expriment1A.java
@@ -4,14 +4,14 @@ public class Expriment1A {
 
         if (number % 2 == 0) {
             if (2 <= number && number <= 5) {
-                System.out.println("Not Wierd");
+                System.out.println("Not Weird");
             } else if (6 <= number && number <= 20) {
-                System.out.println("Wierd");
+                System.out.println("Weird");
             } else {
-                System.out.println("Not Wierd");
+                System.out.println("Not Weird");
             }
         } else {
-            System.out.println("Wierd");
+            System.out.println("Weird");
         }
     }
 }
