76dbc88611254a30309b9e51cb8c9d506ff85c67
SujalChoudhari <sjlchoudhari@gmail.com>
2023-04-25 11:07:54 +0530

    Question 2 added
    With all 3 sub-questions

---
+++ Expriment2A.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Expriment2A{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // get the array
+        System.out.println("Enter length of array: ");
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter elements: ");
+        for(int i=0;i<size;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        // create the resut
+        int[] res = new int[size];
+
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(i != j)
+                res[i] += arr[j];
+            }
+        }
+
+        for(int i=0;i<size;i++){
+            System.out.println(res[i]);
+        }
+
+    }
+}
---
+++ Expriment2B.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Expriment2B {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int[][] arr = new int[5][5];
+
+    System.out.println(
+      "Roll No |   Subject1    |   Subject2    |   Subject3    |"
+    );
+    for (int i = 0; i < 5; i++) {
+      for (int j = 0; j < 4; j++) {
+        arr[i][j] = sc.nextInt();
+        arr[i][4] += arr[i][j];
+      }
+      arr[i][4] -= arr[i][0];
+    }
+
+    System.out.println("	Roll No |   Subject1    |   Subject2    |   Subject3    |	Total	|");
+    System.out.println("	----------------------------------------------------------------------");
+
+    int max = 0;
+    int maxRoll = 0;
+    for (int i = 0; i < 5; i++) {
+      int total = arr[i][4];
+      if (total > max) {
+        max = total;
+        maxRoll = arr[i][0];
+      }
+      System.out.println("\t|"+ arr[i][0] + "\t|\t" +arr[i][1] + "\t|\t"+arr[i][2] + "\t|\t"+arr[i][3] + "\t|\t" + total + "\t|\t");
+    }
+
+    System.out.println("RollNo: " + maxRoll + " scored the maximum. i.e. " + max);
+  }
+}
