264da0ffbb71cee45876261bc7b24aa052c43116
Sujal <sjlchoudhari@gmail.com>
2023-06-05 03:40:28 +0000

    Expriment 8A Improved

--- Expriment8/Expriment8A.java
+++ Expriment8/Expriment8A.java
@@ -1,8 +1,12 @@
-class Student {
-    private int roll;
+import java.util.Scanner;
 
-    public void read(int roll){
-        this.roll = roll;
+class Student {
+    public int rollno;
+
+    public void read(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your rollno");
+        this.rollno = sc.nextInt();
     }
 }
 
@@ -10,32 +14,42 @@ class Test extends Student {
     public int sem1Marks;
     public int sem2Marks;
 
-    public void read(int sem1, int sem2){
-        this.sem1Marks = sem1;
-        this.sem2Marks = sem2;
+
+    public void read(){
+        super.read();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your sem1Marks");
+        this.sem1Marks = sc.nextInt();
+        System.out.println("Enter your sem2Marks");
+        this.sem2Marks = sc.nextInt();
     }
 
 }
 
 interface Sports {
-    public final int score = 5;
+    public int score();
 }
 
 class Result extends Test implements Sports {
     public int total;
 
     public int total(){
-        this.total = this.sem1Marks + this.sem2Marks + this.score;  
+        this.total = this.sem1Marks + this.sem2Marks + score();  
         return this.total;
+    }
+
+    public int score() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your sports score");
+        return sc.nextInt();
     }
 }
 
 public class Expriment8A {
     public static void main(String[] args) {
         Result result  = new Result();
-        result.read(4);
-        result.read(30, 35);
-        System.out.println(result.total());
+        result.read();
+        System.out.println( "Roll No:" + result.rollno + "\nScore: " + result.total());
 
     }
 }
