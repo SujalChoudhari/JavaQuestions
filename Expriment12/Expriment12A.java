051c3eb5ab04d3e7cb2f936bebe8f2729c5c49b8
SujalChoudhari <sjlchoudhari@gmail.com>
2023-06-06 11:49:54 +0530

    Expriment 12 A Added

--- Expriment12/Expriment12A.java
+++ Expriment12/Expriment12A.java
@@ -1,10 +1,103 @@
 package Expriment12;
+import java.awt.event.*;
+
+import javax.net.ssl.TrustManager;
 import javax.swing.*;
-// Swing program to create a login form
-public class Expriment12A {
-    
-    public static void main(String[] args) {
-    
+
+
+class GUI {
+    JFrame main;
+    JButton okButton;
+    JPanel mainContainer;
+    JPanel detailPanel;
+    JTextField usernameField;
+    JPasswordField passwardField;
+    JPanel displayPanel;
+    JLabel displayText;
+
+    String name;
+    String pass;
+
+    public void render(){
+        main = new JFrame("Login");
+        main.setSize(600, 100);
+        main.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainContainer =new JPanel();
+        mainContainer.setLayout(new BoxLayout(mainContainer, BoxLayout.Y_AXIS));
+        detailPanel = new JPanel();
+
+        JLabel loginLabel = new JLabel("Login:");
+        detailPanel.add(loginLabel);
+
+        usernameField = new JTextField(12);
+        detailPanel.add(usernameField);
+
+        JLabel passwardLabel = new JLabel("Password:");
+        detailPanel.add(passwardLabel);
+
+        passwardField = new JPasswordField(12);
+        detailPanel.add(passwardField);
+
+        okButton = new JButton("OK");
+        okButton.addActionListener(new OkButtonHandler());
+        detailPanel.add(okButton);
+        
+        JButton resetButton = new JButton("Reset");
+        resetButton.addActionListener(new ResetButtonHandler());
+        detailPanel.add(resetButton);
+
+
+        displayPanel = new JPanel();
+        displayText = new JLabel("");
+        displayPanel.add(displayText);
+
+        
+        
+        mainContainer.add(detailPanel);
+        mainContainer.add(displayPanel);
+
+        
+        main.add(mainContainer);
+
     }
 
+    public void run(){
+        main.setVisible(true);
+    } 
+
+    class OkButtonHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            name = usernameField.getText();
+            pass = new String(passwardField.getPassword());
+            displayText.setText(name+": "+pass);
+            
+        }
+
+    }
+
+    class ResetButtonHandler implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+           usernameField.setText("");
+           passwardField.setText("");
+            
+        }
+
+    }
+}
+
+// Swing program to create a login form
+public class Expriment12A {
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.render();
+        gui.run();
+    }
+
+   
+  
 }
