e04da11f7c08e2349551433a65caf4a073f124df
Sujal <sjlchoudhari@gmail.com>
2023-06-08 08:44:15 +0000

    Expriment 12B Complete

--- Expriment12/Expriment12B.java
+++ Expriment12/Expriment12B.java
@@ -8,17 +8,23 @@ import javax.swing.plaf.DimensionUIResource;
 
 class Calculator extends JFrame {
 
-  private String[] symbols = { ".", "+", "-", "/", "*", "=", "CE" };
+  private String[] symbols = {
+      "9", "8", "7", "+",
+      "6", "5", "4", "-",
+      "3", "2", "1", "*",
+      "0", "C", ".", "/", 
+              "=",
+  };
 
   private JPanel mainPanel;
   private JPanel displayPanel;
   private JTextField displayTextField;
   private JLabel completeDisplayText;
   private JPanel buttonPanel;
-  private JButton[] numberButtons;
   private JButton[] symbolButtons;
 
-  String lhs,rhs;
+  float lhs, rhs;
+  char functor;
 
   class ButtonListner implements ActionListener {
 
@@ -27,17 +33,63 @@ class Calculator extends JFrame {
       char operator = e.getActionCommand().charAt(0);
       if ((operator >= '0' && operator <= '9') || operator == '.') {
         displayTextField.setText(displayTextField.getText() + operator);
-        completeDisplayText.setText(completeDisplayText.getText() + operator);
+      } else if (operator == '+') {
+        saveAsFirstNumber();
+        functor = operator;
+      } else if (operator == '-') {
+        saveAsFirstNumber();
+        functor = operator;
+      } else if (operator == '*') {
+        saveAsFirstNumber();
+        functor = operator;
+      } else if (operator == '/') {
+        saveAsFirstNumber();
+        functor = operator;
+      } else if (operator == '=') {
+        showTotal();
+      } else if (operator == 'C') {
+        displayTextField.setText("");
+        completeDisplayText.setText("");
+        lhs = 0;
+        rhs = 0;
       }
-      else if(operator == '+'){
-        lhs = displayTextField.getText();
+    }
+
+    public void saveAsFirstNumber() {
+      lhs = Float.parseFloat(displayTextField.getText());
+      displayTextField.setText("");
+    }
+
+    public void saveAsSecondNumber() {
+      rhs = Float.parseFloat(displayTextField.getText());
+    }
+
+    public void showTotal() {
+      saveAsSecondNumber();
+      float answer = 0;
+      switch (functor) {
+        case '+':
+          answer = lhs + rhs;
+          break;
+        case '-':
+          answer = lhs - rhs;
+          break;
+        case '*':
+          answer = lhs * rhs;
+          break;
+        case '/':
+          answer = lhs / rhs;
+          break;
       }
+      lhs = answer;
+      rhs = 0;
+      displayTextField.setText(""+lhs);
     }
   }
 
   Calculator() {
     super("Calculator");
-    this.setSize(350, 350);
+    this.setSize(350, 450);
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
   }
 
@@ -45,7 +97,6 @@ class Calculator extends JFrame {
     mainPanel = new JPanel();
     displayPanel = new JPanel();
     buttonPanel = new JPanel();
-    numberButtons = new JButton[10];
     symbolButtons = new JButton[symbols.length];
     completeDisplayText = new JLabel();
     ButtonListner listner = new ButtonListner();
@@ -53,27 +104,20 @@ class Calculator extends JFrame {
     displayTextField = new JTextField(20);
     displayTextField.setEditable(true);
     displayPanel.add(displayTextField);
-    displayPanel.add(completeDisplayText);
     displayPanel.setMaximumSize(new DimensionUIResource(300, 50));
-
-    for (int i = 0; i < numberButtons.length; i++) {
-      numberButtons[i] = new JButton("" + i);
-      numberButtons[i].setPreferredSize(new DimensionUIResource(60, 60));
-      numberButtons[i].addActionListener(listner);
-      buttonPanel.add(numberButtons[i]);
-    }
 
     // SymbolButtons
     for (int i = 0; i < symbolButtons.length; i++) {
       symbolButtons[i] = new JButton(symbols[i]);
-      symbolButtons[i].setPreferredSize(new DimensionUIResource(60, 60));
+      symbolButtons[i].setPreferredSize(new DimensionUIResource(70, 70));
       symbolButtons[i].addActionListener(listner);
       buttonPanel.add(symbolButtons[i]);
     }
     mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
     mainPanel.add(displayPanel);
+    displayPanel.add(completeDisplayText);
     mainPanel.add(buttonPanel);
-    
+
     this.add(mainPanel);
   }
 }
